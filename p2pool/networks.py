948f453794cd8d98500133fbf36db3758df93cc1
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2015-03-01 11:43:23 -0500

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -61,7 +61,7 @@ nets = dict(
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=True,
         WORKER_PORT=5578,
-        BOOTSTRAP_ADDRS='birdspool.no-ip.org nz.p2pool.geek.nz'.split(' '),
+        BOOTSTRAP_ADDRS='birdspool.no-ip.org nz.p2pool.geek.nz uk.p2pool.geek.nz meziti.nl'.split(' '),
         #ANNOUNCE_CHANNEL='#p2pool',
         VERSION_CHECK=lambda v: True,
     ),    
